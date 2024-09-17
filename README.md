96ba582af51447986a5ef4358aca74897316abb3
Vijay Kumar Pare <vijaypare8@gmail.com>
2024-09-18 00:44:27 +0530

    redme file and sql file upload

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Test Project In Core PHP 
+
+
+
+Portal for admin and users to login. Admin can create users and see reports on the same. Users can login and submit tasks they have been working on.
+
+
+The main part at user side is users should be prompted to change the password every 30 days. 
+
+
+When a user logs in the user should be prompted to change the password the first time which was set by the admin. Once the password change is done. the user should be logged in to the portal.
+## Setup
+Clone the project repository and also find the SQL file (task_system.sql) for the Database.
+
+
+
+## Endpoints
+
+Projetc folder and the path is - 
+
+For Admin - /admin/login.php
+
+For User - /login.php
+
+Admin credentials are available on - \admin\includes\config.php
+
+username - admin
+
+password - admin
---
+++ task_system.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 17, 2024 at 07:00 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `task_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `start_time` timestamp NOT NULL,
+  `stop_time` timestamp NOT NULL,
+  `notes` text,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `last_password_change` datetime DEFAULT NULL,
+  `is_password_default` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `password`, `last_login`, `last_password_change`, `is_password_default`, `created_at`) VALUES
+(1, 'Keshu', 'P', 'test@test.com', '9876543210', '21232f297a57a5a743894a0e4a801fc3', '2024-09-17 23:55:54', '2024-09-17 23:56:03', 0, '2024-09-17 16:40:12'),
+(2, 'test', 'user', 'user@gmail.com', '9876543211', '21232f297a57a5a743894a0e4a801fc3', NULL, NULL, 1, '2024-09-17 17:33:23'),
+(3, 'test2', 'user2', 'user2@gmail.com', '9876543211', '21232f297a57a5a743894a0e4a801fc3', NULL, NULL, 1, '2024-09-17 17:39:06');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
